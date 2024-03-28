5ff105d4873d67b7c072f0df39dc90cbcee63a5e
Saiteja Tokala <saitejatokala@gmail.com>
2024-03-28 16:16:13 +0530

    Update and rename SubsetSumQuires.java to NbricksMcolors.java

--- NbricksMcolors.java
+++ NbricksMcolors.java
@@ -4,17 +4,17 @@ public class NbricksMcolors {
     public static void main(String[] args) {
         NbricksMcolors nbricksMcolors = new NbricksMcolors();
 
-        System.out.println(nbricksMcolors.ways(3, 2, 2));
+        System.out.println(nbricksMcolors.ways(2, 2, 1));
     }
 
     private int ways(int n, int m, int k) {
         Integer[][] dp = new Integer[n + 1][k + 1];
-        return rec(0, n, m, k, dp);
+        return rec(0, k, n, m, dp);
     }
 
-    private int rec(int level, int n, int m, int k, Integer[][] dp) {
-        if (k < 0) {
-            return 1;
+    private int rec(int level, int k, int n, int m, Integer[][] dp) {
+        if(k<0){
+            return 0;
         }
         if (level >= n) {
             if (k == 0) {
@@ -28,14 +28,13 @@ public class NbricksMcolors {
         }
         int same = 0;
         int notSame = 0;
-        if (level != 0) {
-            same = rec(level + 1, n, m, k - 1, dp);
-            if(k>0) {
-                notSame = (m - 1) * rec(level + 1, n, m, k, dp);
-            }
+        if(level!=0) {
+            same = rec(level + 1, k, n, m, dp);
+            notSame = (m - 1) * rec(level + 1, k - 1, n, m, dp);
         }else {
-                notSame = m * rec(level + 1, n, m, k, dp);
+            notSame = (m ) * rec(level + 1, k , n, m, dp);
         }
+
 
         dp[level][k] = same + notSame;
         return same + notSame;
