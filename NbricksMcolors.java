c0a87217584cf3f2a89859d2dd5da021b68c2ed8
Saiteja Tokala <saitejatokala@gmail.com>
2024-03-28 17:01:29 +0530

    Update NbricksMcolors.java

--- NbricksMcolors.java
+++ NbricksMcolors.java
@@ -1,6 +1,9 @@
+
 import java.util.*;
 
 public class Main {
+
+    public static final int MOD = 1000000007;
 
     public static void main(String[] args) {
         Main nbricksMcolors = new Main();
@@ -17,13 +20,11 @@ public class Main {
 
     private long ways(int n, int m, int k) {
         Long[][] dp = new Long[n + 1][k + 1];
-        return rec(0, k, n, m, dp)%1000000007;
+        return rec(0, k, n, m, dp) % MOD;
     }
 
     private long rec(int level, int k, int n, int m, Long[][] dp) {
-        if (k < 0) {
-            return 0;
-        }
+
         if (level >= n) {
             if (k == 0) {
                 return 1;
@@ -37,14 +38,16 @@ public class Main {
         long same = 0;
         long notSame = 0;
         if (level != 0) {
-            same = rec(level + 1, k, n, m, dp);
-            notSame = (m - 1) * rec(level + 1, k - 1, n, m, dp);
+            same = rec(level + 1, k, n, m, dp) % MOD;
+            if (k > 0) {
+                notSame = (m - 1) * rec(level + 1, k - 1, n, m, dp) % MOD;
+            }
         } else {
-            notSame = (m) * rec(level + 1, k, n, m, dp);
+            notSame = (m) * rec(level + 1, k, n, m, dp)%MOD;
         }
 
 
-        dp[level][k] = same + notSame;
-        return same + notSame;
+        dp[level][k] = (same + notSame)%MOD;
+        return dp[level][k] ;
     }
 }
