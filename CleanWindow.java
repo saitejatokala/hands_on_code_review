1c9d6f777a1bb84041e750a25cf86e89aa447286
Saiteja Tokala <saitejatokala@gmail.com>
2024-03-28 19:44:01 +0530

    Update CleanWindow.java

--- CleanWindow.java
+++ CleanWindow.java
@@ -1,10 +1,11 @@
 
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.Scanner;
 
 class Main {
     public static void main(String[] args) {
-      
+
         Scanner sc = new Scanner(System.in);
         int tests = getNextInt(sc);
         for (int t = 0; t < tests; t++) {
@@ -56,6 +57,43 @@ class Main {
         return sc.nextInt();
     }
 
-    record Node(int x, int y) {
-    }
+    static final class Node {
+        private final int x;
+        private final int y;
+
+        Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        public int x() {
+            return x;
+        }
+
+        public int y() {
+            return y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (obj == null || obj.getClass() != this.getClass()) return false;
+            var that = (Node) obj;
+            return this.x == that.x &&
+                    this.y == that.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        @Override
+        public String toString() {
+            return "Node[" +
+                    "x=" + x + ", " +
+                    "y=" + y + ']';
+        }
+    
+        }
 }
